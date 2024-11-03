5587ab11117d92ecd083ab235a800081274fb8d8
loonybtard <MsDos23525@yandex.ru>
2024-11-03 19:35:37 +0300

    WG flags registration

--- src/main/java/ru/overwrite/wggf/WorldGuardGriefFixPlugin.java
+++ src/main/java/ru/overwrite/wggf/WorldGuardGriefFixPlugin.java
@@ -1,5 +1,9 @@
 package ru.overwrite.wggf;
 
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.protection.flags.StateFlag;
+import com.sk89q.worldguard.protection.flags.registry.FlagConflictException;
+import com.sk89q.worldguard.protection.flags.registry.FlagRegistry;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -8,9 +12,20 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import ru.overwrite.wggf.listeners.PlayerListener;
 
+import java.util.HashMap;
+
 public class WorldGuardGriefFixPlugin extends JavaPlugin implements Listener {
 
     private PlayerListener listener;
+
+    public HashMap<String, StateFlag> wgFlags = new HashMap<String, StateFlag>();
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+
+        wgFlagsRegister();
+    }
 
     @Override
     public void onEnable() {
@@ -35,6 +50,32 @@ public class WorldGuardGriefFixPlugin extends JavaPlugin implements Listener {
         reloadConfig();
         sender.sendMessage(color("&f[&6WGGF&f] Конфиг был перезагружен!"));
         return true;
+    }
+
+    private void wgFlagsRegister() {
+        FlagRegistry registry = WorldGuard.getInstance().getFlagRegistry();
+
+        String[] flagsToInit = {
+                "grief-allow-falling", // done
+                "grief-allow-wither", // done
+                "grief-allow-piston",
+                "grief-allow-hopper", // done
+                "grief-allow-explosions", // done
+        };
+
+        for (String flagName : flagsToInit) {
+            try {
+                StateFlag flag = new StateFlag(flagName, false);
+                registry.register(flag);
+                wgFlags.put(flagName, flag);
+            } catch (IllegalStateException e) { // when plugin reloaded via plugin manager
+                wgFlags.put(flagName, (StateFlag) registry.get(flagName));
+            } catch (FlagConflictException e) {
+                // we don't give a fuck
+            }
+        }
+
+
     }
 
     private String color(String string) {
